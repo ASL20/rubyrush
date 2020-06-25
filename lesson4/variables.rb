58f5e9e80a4cec0995724dbdd15d3b971bceb920
Anton <anton.lipich1@gmail.com>
2020-06-25 18:14:12 +0300

    lesson 4 done

---
+++ lesson4/variables.rb
@@ -0,0 +1,14 @@
+hello = "Здравствуйте"
+
+hello2 = nil
+hello2 = "пока, девченки"
+
+devchenki = nil
+
+if hello == "Здравствуйте, господа" || hello == "Привет, пацаны"
+	puts "Привет!"
+else
+	puts "Хам!"
+end
+
+puts "до свидания"
