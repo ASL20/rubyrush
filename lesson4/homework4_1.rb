66684e4269aa6488b4881ff2ab082c39bfbac436
Anton <anton.lipich1@gmail.com>
2020-06-25 18:24:18 +0300

    homework 1 of lesson 4 done

---
+++ lesson4/homework4_1.rb
@@ -0,0 +1,12 @@
+number1 = 172.169
+number2 = 31.514
+
+puts number1, number2
+
+if number1 > number2
+	puts "Наибольшее число: #{number1}"
+elsif number1 == number2
+	puts "Числа равны"
+else
+	puts "Наибольшее число: #{number2}"
+end
