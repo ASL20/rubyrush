2fc5c37a0ad767949dd17f3a97148f27940bfb77
Anton <anton.lipich1@gmail.com>
2020-07-02 15:32:03 +0300

    lesson 6 done

---
+++ lesson6/arrays.rb
@@ -0,0 +1,25 @@
+fruits = ["apple", "orange", "banana"]
+
+puts "Массив: #{fruits.to_s}"
+
+basket = []
+
+basket << "Apple"
+
+basket.push("Mellon")
+
+basket.push("Cherry", "Mango")
+
+puts "Корзина: #{basket}"
+
+puts basket[0]
+
+puts basket[2]
+
+basket.delete("Mellon")
+
+puts basket.to_s
+
+basket.delete_at(0)
+
+puts basket.to_s
