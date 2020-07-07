b02e79d85909e4baa88f02c6bbe64b00176485aa
Anton <anton.lipich1@gmail.com>
2020-07-07 19:13:47 +0300

    homework 1 of lesson 11 done

---
+++ lesson11/homework11_1.rb
@@ -0,0 +1,17 @@
+require_relative 'chameleon'
+
+chameleon = Chameleon.new
+
+chameleon.change_color("красный")
+
+sleep 1
+
+chameleon.change_color("зелёный")
+
+sleep 1
+
+chameleon.change_color("голубой")
+
+sleep 1
+
+chameleon.change_color("жёлтый")
